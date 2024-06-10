b6ce51c8a3139ea17673178e47c1244606b58880
Anupam Raj <70150362+anupamraj0900@users.noreply.github.com>
2024-06-10 14:51:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var rightimageelement = document.querySelector(".img2");
 rightimageelement.setAttribute("src",RightImage);
 
 if(RightImage>leftDiceImage){
-	document.querySelector("h1").innerHTML ="Player 2 Wins! 🚩 " 
+	document.querySelector("h1").innerHTML ="Player 2 Wins! 🚩<br> {REFRESH TO PLAY AGAIN} " 
 
 }
 else if(RightImage==leftDiceImage){
-	document.querySelector("h1").innerHTML="Tie🙁 Try Again!"
+	document.querySelector("h1").innerHTML="Tie🙁 Try Again! <br>{REFRESH TO PLAY AGAIN}"
 }
 else {
-	document.querySelector("h1").innerHTML ="Player 1 Wins! 🚩"
+	document.querySelector("h1").innerHTML ="Player 1 Wins! 🚩<br> {REFRESH TO PLAY AGAIN}"
 }
